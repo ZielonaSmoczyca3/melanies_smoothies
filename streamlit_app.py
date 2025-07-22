d3cf9a95e9f9ac37a2ce3fe238b598ef0729b53f
ZielonaSmoczyca3 <nelly.ciesielska@gmail.com>
2025-07-22 21:00:08 +0200

    Update streamlit_app.py change the data source to https://fruityvice.com/api/fruit/xxx

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,5 +54,5 @@ if ingredients_list:
 
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/apple")
 st.text(smoothiefroot_response.json())
