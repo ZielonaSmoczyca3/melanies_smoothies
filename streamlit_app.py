d6b73158d91e784cf3a1d6ffebac9b16981cf047
ZielonaSmoczyca3 <nelly.ciesielska@gmail.com>
2025-07-22 20:46:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered for: ' + '' + name_on_order + '',  icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+st.text(smoothiefroot_response)
