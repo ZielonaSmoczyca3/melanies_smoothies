7140520a724d25577e79c788f9cb663cbcce3072
ZielonaSmoczyca3 <nelly.ciesielska@gmail.com>
2025-07-22 21:27:33 +0200

    Update streamlit_app.py do not use plurals in fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,10 @@ if ingredients_list:
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
         st.subheader(each_fruit + ' Nutrition information:')
-        fruit = "https://fruityvice.com/api/fruit/" + each_fruit
-        st.write(fruit)
+        # fruit = "https://fruityvice.com/api/fruit/" + each_fruit
+        # st.write(fruit)
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
-        st.text(fruityvice_response.json())
+        # st.text(fruityvice_response.json())
         sf_df = st.dataframe(fruityvice_response.json(), use_container_width = True)
         #st.write(ingredients_string)
 
